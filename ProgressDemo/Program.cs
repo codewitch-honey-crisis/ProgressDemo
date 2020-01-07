28a78c53ec1f8e92a7ed60e33e3640150230e318
codewitch honey crisis <gaztoast@gmail.com>
2020-01-06 19:24:57 -0800

    Add project files.

---
+++ ProgressDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using CU;
+namespace ProgressDemo
+{
+    public class Program
+    {
+		static void Main()
+		{
+			ConsoleUtility.WriteProgressBar(0);
+			for (var i = 0; i <= 100; ++i)
+			{
+				ConsoleUtility.WriteProgressBar(i,true);
+				System.Threading.Thread.Sleep(50);
+			}
+			Console.WriteLine();
+			ConsoleUtility.WriteProgress(0);
+			for (var i = 0; i <= 100; ++i)
+			{
+				ConsoleUtility.WriteProgress(i, true);
+				System.Threading.Thread.Sleep(50);
+			}
+		}
+	}
+}
